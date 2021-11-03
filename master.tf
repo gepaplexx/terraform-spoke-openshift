e6948b37ee536b693ef6683ddbc65db0996e8b7d
Clemens Kaserer <ckaserer@gmail.com>
2021-11-03 10:27:02 +0000

    added master node

---
+++ master.tf
@@ -0,0 +1,23 @@
+resource vsphere_virtual_machine "master" {
+  count            = 3
+  name             = "master${count.index}.${var.spoke_network_name}"
+  resource_pool_id = "${data.vsphere_compute_cluster.cc.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.ds.id}"
+  folder           = "GP/${var.spoke_network_name}"
+
+  num_cpus  = "${var.ocp_master_cpu}"
+  memory    = "${var.ocp_master_memory_mb}"
+  guest_id  = "otherLinux64Guest"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.network.id}"
+    use_static_mac = "true"
+    mac_address = "${var.spoke_mac_prefix}:format('%x', ${count.index + 10}"
+  }
+  
+  disk {
+    label            = "disk0"
+    size             = 128
+  }
+
+}
